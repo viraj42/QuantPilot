b10e8d67683c921860879cb55e526d0c27625d12
viraj42 <virajpadaval42@gmail.com>
2026-04-14 12:51:05 +0530

    Resolved some bugs

--- Frontend/my-app/vite.config.js
+++ Frontend/my-app/vite.config.js
@@ -3,19 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: "./",   // ✅ IMPORTANT
+  base: "/",   // ✅ IMPORTANT
   plugins: [
     react(),
     tailwindcss(),
-  ],
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://10.97.43.39:8080',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  ]
 })
